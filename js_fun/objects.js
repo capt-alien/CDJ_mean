8db4ef515ba1d5d0cffd59a2a20df06892071e02
capt-alien <eric.botcher@presidio.edu>
2019-09-30 13:50:57 -0700

    finished users

--- js_fun/objects.js
+++ js_fun/objects.js
@@ -21,3 +21,37 @@ function printer(arr){
 }
 
 printer(students)
+
+
+// Challenge 2
+// Write a function that accepts an object of users divided into employees and
+// managers, and display the number of characters per employee/manager's name, as
+//  shown below, and logs the information to the console.
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
+
+ function users_arr(users){
+     for (type in users){
+         console.log(type.toUpperCase())
+         for (var i = 0; i < users[type].length; i++){
+           var first = users[type][i].first_name;
+           var last = users[type][i].last_name;
+           console.log((i+1)+" - "+first.toUpperCase()+" "+last.toUpperCase()+" - "+(first.length+last.length)
+           );
+         }
+         }
+     }
+
+
+users_arr(users);
