cf98fd1a940384b7f1e292b98bff4c871844cd37
capt-alien <eric.botcher@presidio.edu>
2019-10-03 13:30:05 -0700

    counter working

--- express_stuff/counter/server.js
+++ express_stuff/counter/server.js
@@ -1,19 +1,44 @@
 const express = require("express");
-const app = express()
+const session = require('express-session');
+const app = express();
 
 // Pluggins
-// Plugins
+
 app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 
 // Routes
 app.get('/', (req, res) => {
-    console.log("hitting index")
-   res.render('index');
+    if( req.session.counter){
+        req.session.counter += 1
+    }
+    else{
+        req.session.counter = 1
+    }
+    obj = {'counter': req.session.counter}
+    console.log(req.session)
+   res.render('index', obj);
    // res.send("Sub bub!")
 });
+
+app.get('/two', (req, res) => {
+    req.session.counter += 1;
+    res.redirect('/');
+});
+
+//clear
+app.get('/clear', (req, res) => {
+    req.session.counter = 0;
+    res.redirect('/');
+});
+
 
 
 app.listen(6960, () => console.log("listening on port 6960"));
