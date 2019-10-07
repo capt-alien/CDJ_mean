c12019eb79e755d863cd9e1157425b8e43bf7141
capt-alien <eric.botcher@presidio.edu>
2019-10-07 13:39:57 -0700

    routes writen

--- mongo_db/quoting/server.js
+++ mongo_db/quoting/server.js
@@ -1,21 +1,69 @@
 const express = require("express");
+// const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 
-// middleware
+mongoose.connect('mongodb://localhost3030/quotes', {useNewUrlParser: true});
+const QuoteSchema = new mongoose.Schema({
+                                         name: {type: String},
+                                         quote: {type: String},
+                                            }, {timestamps: true})
 
+const Quote = mongoose.model('Quote', QuoteSchema);
+
+// create an object to that contains methods for mongoose to interface with MongoDB
+
+// middleware
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', __dirname + '/views');
+// app.use(session({
+//   secret: 'keyboardkitteh',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { maxAge: 60000 }
+// }))
 
 
 // routes
-app.get('/', (request, response) => {
-   response.send("Hello Express");
+app.get('/', (req, res) => {
+   res.render('index')
 });
 
+// submit route
+app.post('/quoting', function(req, res){
+    console.log(req.body);
+    Quote.create(req.body)
+    .than(function(data){
+        console.log(data);
+        res.redirect('/quotes')
+    })
+    .catch(function(){
+        console.log(err);
+        res.redirect('/')
+    })
+})
 
 
-
-
-
+// quotes route
+app.get('/quotes', function(req, res){
+    Quote.find()
+        .then(function(data){
+            console.log(data);
+            res.render('quotes', {quotes: data})
+        })
+        .catch(function(err){
+            console.log(err);
+            res.render('quotes');
+        })
+})
 
 
 // Bottom
