57f56e563eeea53a13f501232a88f99fa9fc562b
capt-alien <eric.botcher@presidio.edu>
2019-10-07 14:30:32 -0700

    finished quotes

--- mongo_db/quoting/server.js
+++ mongo_db/quoting/server.js
@@ -1,47 +1,39 @@
 const express = require("express");
-// const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
 const app = express();
 
-
-mongoose.connect('mongodb://localhost3030/quotes', {useNewUrlParser: true});
-const QuoteSchema = new mongoose.Schema({
-                                         name: {type: String},
-                                         quote: {type: String},
-                                            }, {timestamps: true})
-
-const Quote = mongoose.model('Quote', QuoteSchema);
+mongoose.connect('mongodb://localhost:27017/quotes', {useNewUrlParser: true});
+let QuoteSchema = new mongoose.Schema({
+    name: {type: String},
+    quote: {type: String}
+// })
+}, {timestamps: true})
 
 // create an object to that contains methods for mongoose to interface with MongoDB
+let Quote = mongoose.model('Quote', QuoteSchema);
+
 
 // middleware
-app.use(express.static(__dirname + "/static"));
-app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-// app.use(session({
-//   secret: 'keyboardkitteh',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { maxAge: 60000 }
-// }))
+
 
 
 // routes
 app.get('/', (req, res) => {
-   res.render('index')
+    res.render('index')
 });
 
 // submit route
-app.post('/quoting', function(req, res){
+app.post('/submit', function(req, res){
     console.log(req.body);
     Quote.create(req.body)
-    .than(function(data){
+    .then(function(data){
         console.log(data);
         res.redirect('/quotes')
     })
@@ -55,14 +47,14 @@ app.post('/quoting', function(req, res){
 // quotes route
 app.get('/quotes', function(req, res){
     Quote.find()
-        .then(function(data){
-            console.log(data);
-            res.render('quotes', {quotes: data})
-        })
-        .catch(function(err){
-            console.log(err);
-            res.render('quotes');
-        })
+    .then(function(data){
+        // console.log(data);
+        res.render('quotes', {quotes: data})
+    })
+    .catch(function(err){
+        console.log(err);
+        res.render('quotes');
+    })
 })
 
 
