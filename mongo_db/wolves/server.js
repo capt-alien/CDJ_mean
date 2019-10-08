29ec4dd2bad81d19ad6a86ad9703988818a7a790
capt-alien <eric.botcher@presidio.edu>
2019-10-08 15:18:56 -0700

    something

--- mongo_db/wolves/server.js
+++ mongo_db/wolves/server.js
@@ -5,92 +5,97 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Model
-mongoose.connect('mongodb://localhost:27017/wolves',
-                                { useUnifiedTopology: true,
-                                    useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/wolves', { useUnifiedTopology: true, useNewUrlParser: true});
+
 let WolfSchema = new mongoose.Schema({
-    name: {type: String},
-    age: {type: Number, min:0, max:30},
-// })
-}, {timestamps: true})
+        name: {type: String},
+        age: {type: Number, min:0, max:30},    // })
+    }, {timestamps: true})
 
-// create an object to that contains methods for mongoose to interface with MongoDB
-let Wolf = mongoose.model('Wolf', WolfSchema);
+    // create an object to that contains methods for mongoose to interface with MongoDB
+    let Wolf = mongoose.model('Wolf', WolfSchema);
 
-// middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/static"));
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
+    // middleware
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static(__dirname + "/static"));
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
 
 
-// routes
-app.get('/', (req, res) => {
-    Wolf.find()
-    .then(function(data){
-        res.render('index', {wolves: data})
+
+    // routes
+    app.get('/', (req, res) => {
+        Wolf.find()
+            .then(function(allWolves){
+                res.render('index', {wolves: allWolves})
+            })
+            .catch(function(err){
+                console.log(err);
+                res.render('index')
+            })
+    });
+
+    // new
+    app.get('/wolves/new', (req, res) => {
+        res.render('new')
     })
-    .catch(function(err){
-        console.log(err);
-        res.render('index')
+    // new post
+    app.post('/wolves/submit', (req, res) =>{
+        console.log(req.body);
+        Wolf.create(req.body)
+            .then(function(newWolf){
+                console.log("Puppers Created")
+                res.redirect('/')
+            })
+            .catch(function(err){
+                console.log("Puppers failed to create")
+                res.redirect('/')
+            })
     })
-});
 
-// new
-app.get('/wolves/new', (req, res) => {
-    res.render('new')
-})
-// new post
-app.post('/wolves/submit', (req, res) =>{
-    console.log(req.body);
-    Wolf.create(req.body);
-    console.log("Puppers Created")
-    res.redirect('/')
-})
+    // SHOW
+    app.get('/wolves/:id', (req, res) => {
+        Wolf.findOne({_id:req.params.id})
+        .then( function(data){
+            res.render('wolf', {wolf: data})
+        })
+        .catch(function(err){
+            console.log(err);
+            res.redirect('/')
+        })
+    })
 
-// SHOW
-app.get('/wolves/:id', (req, res) => {
-    Wolf.findOne({_id:req.params.id})
-    .then( function(data){
-        res.render('wolf', {wolf: data})
+    // EDIT
+    app.get('/wolves/edit/:id', (req, res)=>{
+        Wolf.findOne({_id:req.params.id})
+        .then( function(data){
+            res.render('edit', {wolf: data})
+        })
+        .catch(function(err){
+            console.log(err);
+            res.redirect('/wolves/:id')
+        })
     })
-    .catch(function(err){
-        console.log(err);
-        res.redirect('/')
-    })
-})
 
-// EDIT
-app.get('/wolves/edit/:id', (req, res)=>{
-    Wolf.findOne({_id:req.params.id})
-    .then( function(data){
-        res.render('edit', {wolf: data})
-    })
-    .catch(function(err){
-        console.log(err);
-        res.redirect('/wolves/:id')
-    })
-})
-
-// ???Put method????
-app.post('/wolves/edit/:id/submit', (req, res)=>{
-    Wolf.updateOne({_id:req.params.id},{
-        name:req.body.name,
-        age: req.body.age
-    })
-    .then(function(){
-        res.redirect('/')
-    })
-    .catch(function(err){
-        console.log(err);
-        res.redirect('/')
-    })
-}  )
+    // ???Put method????
+    app.post('/wolves/edit/:id/submit', (req, res)=>{
+        Wolf.updateOne({_id:req.params.id},{
+            name:req.body.name,
+            age: req.body.age
+        })
+        .then(function(data){
+            console.log('edited a wolf', data);
+            res.redirect('/')
+        })
+        .catch(function(err){
+            console.log(err);
+            res.redirect('/')
+        })
+    }  )
 
 
-// delete
-app.post('/wolves/delete/:id', (req, res)=>{
+    // delete
+    app.post('/wolves/delete/:id', (req, res)=>{
         Wolf.remove({_id:req.params.id})
         .then(function(){
             console.log("wolf object euthanized")
@@ -102,7 +107,7 @@ app.post('/wolves/delete/:id', (req, res)=>{
 
         })
 
-})
+    })
 
 
-app.listen(3030, () => console.log("listening on 3030"));
+    app.listen(3030, () => console.log("listening on 3030"));
